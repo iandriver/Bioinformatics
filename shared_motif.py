663640c48b932273b5badadcf61264c19f55ae79
idbedead <driver.ian@gmail.com>
2013-01-06 21:41:31 -0500

    Update shared_motif.py

--- shared_motif.py
+++ shared_motif.py
@@ -1,3 +1,4 @@
+import sys
 import collections
 import re
 seq_list = []
@@ -7,11 +8,11 @@ matchl =[]
 done = []
 final_motif_list =[]
 def split_len(seq):
-  split = []
+	split = []
 	fin_split =[]
 	slen = [seq[start:end] for start in range(len(seq)-1) for end in range(2,len(seq)+1)]
 	for s in slen:
-		if s != '' and len(s)>5 and len(s)<251:
+		if s != '' and len(s)>15 and len(s)<251:
 			split.append(s)
 	return list(set(split))	
 	
@@ -19,7 +20,8 @@ def split_len(seq):
 ln = 1
 cnt = 0
 patte = []
-with open("rosalind_mot.txt", "r") as f:
+match_arr = []
+with open("rosalind_lcs.txt", "r") as f:
 	for lin in f:
 		seq_list.append(lin.strip())
 	patt = split_len(seq_list[0])  	
@@ -28,16 +30,16 @@ with open("rosalind_mot.txt", "r") as f:
 		if other < 2:
 			pattern = patt
 		else:
-			for shor in match_list.items():
-				patte.append(shor[0])
-			pattern = patte	
+			patter = sorted(match_arr[other-1], key = len)
+			pattern = patter[-20:]
 		for p in pattern:
 			if seq_list[0] != seq_list[other]:
 				if p in seq_list[other]:
 					match.append(p)
-					match_list = collections.Counter(match)	
+					match_list = collections.Counter(match)
+		match_arr.append(set(match))		
 						
-	print match_list								
+	print len(match_arr)							
 	for c in match_list.items():
 		if c[1] == len(seq_list)-1:
 			final_motif_list.append(c[0])
